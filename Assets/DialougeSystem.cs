bd7d5f1fcfed9c8a3c82261686891942c9eb0e57
Safayet Anowar Shurid <Shurid16@gmail.com>
2021-03-07 10:39:15 +0600

    Initial commit

---
+++ Assets/DialougeSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialougeSystem : MonoBehaviour
+{
+    [TextArea]
+    public List<string> lines;
+    public Text text;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            showText();
+        }
+    }
+
+    public void showText()
+    {
+        StartCoroutine(showTextRoutine(0));
+    }
+
+    IEnumerator showTextRoutine(int index)
+    {
+
+        char[] letters = lines[index].ToCharArray();
+        foreach(char c in letters)
+        {
+            text.text += c;
+            yield return new WaitForSeconds(0.05f);
+        }
+       
+
+
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+ 
+    private Rigidbody2D rb;
+    private Animator animator;
+
+   // public NPC currentNPC;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk();
+    }
+
+    private void Walk()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector3 movePos = new Vector3(x, y);
+
+        if (movePos != Vector3.zero)
+        {
+            animator.SetFloat("Horizontal", movePos.x);
+            animator.SetFloat("Vertical", movePos.y);
+            animator.SetBool("isWalking", true);
+            //transform.position += movePos * speed * Time.deltaTime;
+
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+        rb.velocity = movePos * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collided");
+
+    }
+}
